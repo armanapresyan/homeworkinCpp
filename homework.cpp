16267d731de3b0582e696800f775ee61a4658a83
armanapresyan <99826177+armanapresyan@users.noreply.github.com>
2023-06-08 00:49:43 +0400

    Add files via upload

--- homework.cpp
+++ homework.cpp
@@ -1,84 +1,124 @@
 //1
+
 #include <iostream>
-int main()
-{
-int number{0};
-std::cout<<"number: ",std::cin>>number;
-int sum{0};
-while(number != 0){
-    sum += number%10;
-    number = (number - (number% 10))/10;
+#include <cstring>
+
+char* to_upper_case(char *str){
+    for(int p = 0; p < strlen(str); ++p ){
+        if( *(str + p) >= 97 && *(str + p) <= 122){
+            *(str + p)= *(str + p) - 32;
+        }     
+    } 
+    return str;  
 }
-std::cout<<"digit sum: "<<sum<<std::endl;
-return 0;
-} 
+
+
+int main()
+{ 
+ char string[10];
+ std::cout<<"input  text: ";std::cin>>string;
+ char* newArr = to_upper_case(string);
+ std::cout<<"upper case text: "<<newArr;
+ return 0;
+
+}
+
 
 //2
 
 #include <iostream>
-int main()
-{
-int number{0};
-std::cout<<"number: ",std::cin>>number;
-int sum{0};
-int p{1};
-while(number != 0){
-    p *= number%10;
-    sum += number%10;
-    number = (number - (number% 10))/10;
+#include <cstring>
+
+char* strcat(char* s1, char* s2){
+    int q{0};
+    int sizeStr1 = strlen(s1);
+    for(int i = sizeStr1; i <= 20; ++i){
+        *(s1 + i) = *(s2 + q);
+        q++;
+    }
+    
+    return s1;
 }
-std::cout<<"digits multiplication - sum: "<<p-sum<<std::endl;
-return 0;
-} 
+      
+int main()
+{ 
+ char str1[20];
+ char str2[10];
+ std::cout<<"input first string: "; std::cin>>str1;
+ std::cout<<"input second string: "; std::cin>>str2;
+ char* newarrstr = strcat(str1, str2);
+ std::cout<<"input string concatenates: "<<newarrstr;
+ return 0;
+ 
+}
+
 
 //3
 
 #include <iostream>
-int numberOddCounts(int a, int b);
-int main()
-{
-  int a{0};
-  int b{0};
-  std::cout<<"a: "; std::cin>>a;
-  std::cout<<"b: "; std::cin>>b;
-  if(a < b){
-    int count = numberOddCounts(a , b);
-    std::cout<<"odd count: "<<count<<std::endl;
-    }else{
-        std::cout<<"a must be less than b"<<std::endl;
-        return -1;
+#include <cstring>
+
+char* strset(char* s1) {
+    int sizeStr1 = strlen(s1);
+    for (int i = 0; i < sizeStr1; ++i) {
+        if (*(s1 + i) == 's') {
+            for (int j = sizeStr1; j > i; --j) {
+                *(s1 + j + 1) = *(s1 + j);    
+            }
+            *(s1 + i) = 'c';
+            *(s1 + i + 1) = 'h';
+            sizeStr1 += 2; 
+        }
     }
-  return 0;
+
+    return s1;
 }
-int numberOddCounts(int a, int b)
-{
- int count = 0;
- for(int i = a; i <= b; ++i){
-     if(i % 2 != 0){
-         std::cout<<i<<std::endl;
-         count++;
-         }
-     }
-     return count;
+
+int main() {
+    char str1[20];
+    std::cout << "Input string: ";
+    std::cin >> str1;
+
+    char* newarrstr = strset(str1);
+    std::cout << newarrstr << std::endl;
+    return 0;
 }
+
 
 //4
 
 
 #include <iostream>
-#include <vector>
+#include <cstring>
 
 
-int main()
-{ 
-  int count{0};
-  std::vector<int> arr = {4, 1, 3, 3, 4};
-  for(int i = 1; i < arr.size(); ++i){
-      if(arr[i] % i == 0){
-          count++;
-          }
-      }
-  std::cout<<"the number of array members that are divisible by their index without remainder: "<<count<<std::endl;
-  return 0;
+char* strmove(char* s, int n) {
+    int size = strlen(s);
+    char p[20];
+    for(int i = 0; i < size - n; ++i ){
+        *(p + i) = *(s + i);
+    }
+
+    for(int i = 0; i < n; ++i){
+      *(s + i) = *(s + size - n + i);
+    }
+
+    for(int i = n; i < size; ++i){
+        *(s+i) = *(p + i - n);
+    }
+    return s;
 }
+
+
+int main() {
+    char str[20];
+    std::cout << "Input string: ";
+    std::cin>>str;
+
+    char* newStr = strmove(str, 2);
+    std::cout <<"output move string: "<< newStr;
+
+    return 0;
+}
+
 
